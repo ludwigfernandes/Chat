8cebf986dd08b3e78d25805b646c5c065dbcdb7a
Ludwig Fernandes <98396168+ludwigfernandes@users.noreply.github.com>
2024-05-27 11:59:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/chat/MainActivity.kt
@@ -0,0 +1,121 @@
+package com.example.chat
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.chat.databinding.ActivityMainBinding
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthMissingActivityForRecaptchaException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var storedVerificationId: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.btnGetOTP.setOnClickListener {
+            val etPhoneNumber=binding.etPhoneNumber.text
+            if (etPhoneNumber.isNotEmpty() && etPhoneNumber.length == 10) {
+                val phoneNumber = "+91 $etPhoneNumber"
+                sendCode(phoneNumber)
+            } else {
+                Toast.makeText(this, "Enter valid Phone Number!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.btnSubmit.setOnClickListener {
+            if (binding.etOTP.text.isNotEmpty()) {
+                verifyCode(binding.etOTP.text.toString())
+            } else {
+                Toast.makeText(this, "Enter valid OTP!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    private fun verifyCode(code: String) {
+        val credential = PhoneAuthProvider.getCredential(storedVerificationId, code)
+        signInWithPhoneAuthCredential(credential)
+    }
+
+    private fun sendCode(phoneNumber: String) {
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(phoneNumber)
+            .setTimeout(60L, TimeUnit.SECONDS)
+            .setActivity(this)
+            .setCallbacks(callbacks)
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+    }
+
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+
+                    val user = task.result?.user
+                } else {
+                    // Sign in failed, display a message and update the UI
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                        // The verification code entered was invalid
+                    }
+                    // Update UI
+                }
+            }
+    }
+
+    private val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+            Log.d(TAG, "onVerificationCompleted:$credential")
+            signInWithPhoneAuthCredential(credential)
+        }
+
+        override fun onVerificationFailed(e: FirebaseException) {
+            Log.w(TAG, "onVerificationFailed", e)
+
+            if (e is FirebaseAuthInvalidCredentialsException) {
+                // Invalid request
+            } else if (e is FirebaseTooManyRequestsException) {
+                // The SMS quota for the project has been exceeded
+            } else if (e is FirebaseAuthMissingActivityForRecaptchaException) {
+                // reCAPTCHA verification attempted with null Activity
+            }
+
+            // Show a message and update the UI
+        }
+
+        override fun onCodeSent(
+            verificationId: String,
+            token: PhoneAuthProvider.ForceResendingToken,
+        ) {
+            Log.d(TAG, "onCodeSent:$verificationId")
+
+            storedVerificationId = verificationId
+        }
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+}
